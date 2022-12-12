bab3b73567dc1513f2a4584de5d4021711f8e988
daudi13 <davouma13@gmail.com>
2022-12-12 08:30:55 +0300

    Integrate index css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import BookstoreContext from './BookstoreContex';
 import App from './App';
+import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
