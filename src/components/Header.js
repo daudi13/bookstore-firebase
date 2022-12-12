9d68387030a09bd449d5439108994df108072ee2
daudi13 <davouma13@gmail.com>
2022-12-12 17:43:38 +0300

    Add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { makeStyles } from 'tss-react/mui';
+import BookstoreContext from '../BookstoreContex';
 
-const Header = () => {
+export default function Header() {
+
+  const { user } = BookstoreContext;
+  const useStyle = makeStyles()(() => ({
+    Appbar: {
+      display: "flex",
+      alignItems: "center",
+      width: "100%"
+    }
+  }))
+
+  const { classes } = useStyle();
   return (
-    <div>Header</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" className={classes.Appbar}>
+            BookStoreCMS
+          </Typography>
+          {user && <Button color="inherit">Login</Button>}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-
-export default Header
