9f0b906feec8ee656c9d0bca41ebbcd33d8f4899
daudi13 <davouma13@gmail.com>
2022-12-13 15:35:47 +0300

    Add a variable to hold auth var

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,11 @@
 import { initializeApp } from "firebase/app";
+import { getAuth} from "firebase/auth"
 import firebaseConfig from "./firebaseConfig";
 import { getFirestore } from "firebase/firestore";
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const db = getFirestore(app);
 
-export default db;
+const firebaseEngine = { db, auth };
+export default firebaseEngine;
