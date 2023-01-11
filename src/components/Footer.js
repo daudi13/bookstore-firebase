18ba391dcd56d9d8db433fcb5668e436343c6664
daudi13 <davouma13@gmail.com>
2023-01-11 16:29:14 +0300

    Add footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,26 @@
+import { Link, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react'
+import { makeStyles } from 'tss-react/mui'
 
 const Footer = () => {
+
+  const date = new Date();
+
+  const useStyle = makeStyles()((theme) => ({
+    footer: {
+      padding: "10px", 
+      background: "grey",
+    }
+  }));
+
+  const { classes } = useStyle();
   return (
-    <div>Footer</div>
+    <Box className={classes.footer}>
+      <Typography variant='caption' gutterBottom >
+        &#169; <Link href="">David Ouma</Link> | {date.getFullYear()} 
+      </Typography>
+    </Box>
   )
 }
 
