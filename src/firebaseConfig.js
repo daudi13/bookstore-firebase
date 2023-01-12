e6eb265585a37a8a4f5b77188ed62055a4c20787
daudi13 <davouma13@gmail.com>
2023-01-12 20:48:16 +0300

    Inject firebaseConfig directly

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_API_ID ,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID 
+  apiKey: "AIzaSyCNdNih0aqqiUpWU7Y0VSgMyPWRJc4vZyo",
+  authDomain: "bookstore-c5671.firebaseapp.com",
+  projectId: "bookstore-c5671",
+  storageBucket: "bookstore-c5671.appspot.com",
+  messagingSenderId: "107126883642",
+  appId: "1:107126883642:web:f7b4ed9737ddf6df683c03" ,
+  measurementId: "G-YH8N44DPK6" 
 }
 
 export default firebaseConfig;
